4de495326e0c908aa7d9df68c54675abc9197de1
masaki110 <masaki.hmym@google.com>
2022-02-04 09:25:03 +0900

    Update for undecidale instances.

--- src/ppx_fillup.ml
+++ src/ppx_fillup.ml
@@ -18,46 +18,54 @@ let rec apply_holes n exp =
 let evar ident =
   Ast_helper.Exp.ident (Location.mknoloc (Longident.Lident (Ident.name ident)))
 
-type instance_polymorphism = Poly of int * Ident.t | Nonpoly of Ident.t
+type instance_polymorphism = Poly of int * Ident.t | Mono of Ident.t
 
 let rec match_instance env holety ident instty =
   let instty = Ctype.repr @@ Ctype.expand_head env instty in
   match instty.desc with
   | Tarrow (_, _, ret, _) -> (
-      if Ctype.matches env holety instty then Some (Nonpoly ident)
+      if Ctype.matches env holety instty then Some (Mono ident)
       else
         match match_instance env holety ident ret with
-        | Some (Nonpoly ident) -> Some (Poly (1, ident))
+        | Some (Mono ident) -> Some (Poly (1, ident))
         | Some (Poly (n, ident)) -> Some (Poly (n + 1, ident))
         | None -> None)
   | _ ->
       (* prerr_endline "other"; *)
-      if Ctype.matches env holety instty then Some (Nonpoly ident) else None
+      if Ctype.matches env holety instty then Some (Mono ident) else None
 
 let is_instance (desc : Types.value_description) =
   List.exists (fun attr -> attr.attr_name.txt = "instance") desc.val_attributes
 
 let instances = ref []
 
-let resolve_instances ty env =
+let resolve_instances (texp : Typedtree.expression) =
   let rec find_instances (instances : (Ident.t * Types.value_description) list)
       =
     match instances with
     | (ident, desc) :: rest -> (
-        match match_instance env ty ident desc.val_type with
+        match match_instance texp.exp_env texp.exp_type ident desc.val_type with
         | Some i -> i :: find_instances rest
         | None -> find_instances rest)
     | [] -> []
   in
   find_instances !instances
 
-let lookup_hole self (super : Untypeast.mapper) (texp : Typedtree.expression)
-    attr =
+let fillup_hole self (super : Untypeast.mapper) attr
+    (texp : Typedtree.expression) =
   match attr with
   | { Parsetree.attr_name = { txt = "HOLE"; _ }; attr_loc = loc; _ } -> (
-      match resolve_instances texp.exp_type texp.exp_env with
-      | Nonpoly ident :: _ -> evar ident
-      | Poly (n, ident) :: _ -> [%expr [%e apply_holes n @@ evar ident]]
+      match resolve_instances texp with
+      (* | Mono ident :: _ -> evar ident
+         | Poly (n, ident) :: _ -> [%expr [%e apply_holes n @@ evar ident]]
+         | [] ->
+             Location.raise_errorf ~loc "Instance not found:%a" Printtyp.type_expr
+               texp.exp_type) *)
+      | [ Mono ident ] -> evar ident
+      | [ Poly (n, ident) ] -> [%expr [%e apply_holes n @@ evar ident]]
+      | _ :: _ ->
+          Location.raise_errorf ~loc "Undecidable instance:%a"
+            Printtyp.type_expr texp.exp_type
       | [] ->
           Location.raise_errorf ~loc "Instance not found:%a" Printtyp.type_expr
             texp.exp_type)
@@ -70,18 +78,22 @@ let untyper =
     expr =
       (fun self (texp : Typedtree.expression) ->
         match (texp.exp_attributes, texp.exp_extra) with
-        | attr :: _, _ -> lookup_hole self super texp attr
-        | _, (_, _, attr :: _) :: _ -> lookup_hole super self texp attr
+        | attr :: _, _ -> fillup_hole self super attr texp
+        | _, (_, _, attr :: _) :: _ -> fillup_hole super self attr texp
         (* | _, _, -> *)
         | _ -> super.expr self texp);
-    (* module_type =
-      (fun self (tmty : Typedtree.module_type) ->
-        match tmty.mty_desc with
-        (* |  -> super.module_type self tmty *)
-        | _ ->
-            prerr_endline "here";
-            super.module_type self tmty); *)
   }
+
+(* let alert_filled (super:Ast_mapper.mapper) (self:Ast_mapper.mapper) (exp:Parsetree.expression) =
+   match exp with
+   | {pexp_desc=_; pexp_attributes={attr_name={txt="FILLED";_};_}::_;_} ->
+     let exp = super.expr self exp in
+     Format.asprintf "Filled: (%a)" Ocaml_common.Pprintast.expression exp
+   | _ ->
+     if List.exists (fun a -> a.Parsetree.attr_name.txt = "HOLE") exp.pexp_attributes then
+       Location.raise_errorf ~loc:exp.pexp_loc "Can't fill"
+     else
+       super.expr self exp *)
 
 let rec loop_typer_untyper str =
   Compmisc.init_path ();
@@ -158,17 +170,6 @@ class replace_hashhash =
           Ast_helper.Exp.apply ~loc:exp.pexp_loc ~attrs:exp.pexp_attributes
             (this#expression arg1)
             [ (Nolabel, make_hole loc); (Nolabel, this#expression arg2) ]
-      (* | Pexp_apply
-          ( {
-              pexp_desc = Pexp_ident { txt = Lident "##"; _ };
-              pexp_loc = loc_hole;
-              _;
-            },
-            [ (_, arg1) ] ) ->
-          let loc = loc_hole in
-          Ast_helper.Exp.apply ~loc:exp.pexp_loc ~attrs:exp.pexp_attributes
-            (this#expression arg1)
-            [ (Nolabel, make_hole loc) ] *)
       | _ -> super#expression exp
   end
 
@@ -180,26 +181,21 @@ let transform str =
   List.iter
     (fun (ident, (_desc : Types.value_description)) ->
       (* match desc.val_type.desc with
-      | Tvar a -> output_string out (Ident.name ident ^ " " ^ (Option.get a) ^ "\n")
-      | _ -> output_string out (Ident.name ident ^ "\n") *)
+         | Tvar a -> output_string out (Ident.name ident ^ " " ^ (Option.get a) ^ "\n")
+         | _ -> output_string out (Ident.name ident ^ "\n") *)
       output_string out (Ident.name ident ^ "\n"))
     !instances;
   close_out out;
   (* print end *)
   loop_typer_untyper str
 
-(* let expand ~loc ~path:_ = make_hole loc *)
-
 (* Declaration of extension [%HOLE] : https://tarides.com/blog/2019-05-09-an-introduction-to-ocaml-ppx-ecosystem *)
-let hole =
-  Ppxlib.Extension.declare
-    "HOLE"
-    Ppxlib.Extension.Context.expression
+let hole_extension =
+  Ppxlib.Extension.declare "HOLE" Ppxlib.Extension.Context.expression
     Ppxlib.Ast_pattern.(pstr nil)
     (fun ~loc ~path:_ -> make_hole loc)
 
 let () =
-  Ppxlib.Driver.register_transformation
-    ~extensions:[hole]
+  Ppxlib.Driver.register_transformation ~extensions:[ hole_extension ]
     ~instrument:(Ppxlib.Driver.Instrument.make ~position:After transform)
     "ppx_fillup"
