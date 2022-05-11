58d344e69706bf2d55f76d4133e9e4874311860b
masaki110 <masaki.hmym@google.com>
2022-05-12 00:51:34 +0900

    add test cases

--- test/test.ml
+++ test/test.ml
@@ -16,20 +16,27 @@ let test_show _ =
   assert_equal ~printer "1.23" show ## 1.23;
   assert_equal ~printer "abc" show ## "abc"
 
+let (show_option [@instance]) =
+ fun inst -> function None -> "None" | Some i -> "Some " ^ inst i
+
 let (show_list [@instance]) =
- fun inst xs ->
-  match xs with [] -> "" | _ :: _ -> String.concat ", " (List.map inst xs)
+ fun inst -> function [] -> "" | xs -> String.concat ", " (List.map inst xs)
 
-let test_show_list _ =
+let test_show_polymorphic _ =
+  assert_equal "Some 123" show ## (Some 123);
+  (* assert_equal "None" show ## None; *)
   assert_equal "123, 456, 789" show ## [ 123; 456; 789 ];
-  assert_equal "1.23, 4.56, 7.89" show ## [ 1.23; 4.56; 7.89 ];
-  assert_equal "abc, def, ghi" show ## [ "abc"; "def"; "ghi" ]
+  assert_equal "1.23, 4.56, 7.89" show ## [ 1.23; 4.56; 7.89 ]
 
-let float_int[@instance] = float_of_int 
-  
+let test_local_declearation _ =
+  let (show_bool [@instance]) = string_of_bool in
+  assert_equal "true" show ## true
+
+let (float_int [@instance]) = float_of_int
+
 let average : int list -> float =
-  fun xs ->
-    let sum, count =
+ fun xs ->
+  let sum, count =
     xs |> List.fold_left (fun (sum, count) x -> (sum + x, succ count)) (0, 0)
   in
   [%HOLE] sum /. [%HOLE] count
@@ -41,6 +48,10 @@ type student = { id : int; name : string } [@@deriving show, eq, ord, fillup]
 
 let tests =
   "Test fillup"
-  >::: [ "test show" >:: test_show; "test show list" >:: test_show_list ]
+  >::: [
+         "test show" >:: test_show;
+         "test show polymorphic" >:: test_show_polymorphic;
+         "test local declearation" >:: test_local_declearation;
+       ]
 
 let _ = run_test_tt_main tests
