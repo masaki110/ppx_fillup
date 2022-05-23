7a187e2b03a38ab2eb37ae1b911f4728638906aa
masaki110 <masaki.hmym@google.com>
2022-05-23 21:53:43 +0900

    make instances using Env.fold

--- src/main.ml
+++ src/main.ml
@@ -2,6 +2,7 @@ open Parsetree
 
 let name_dummy_module = "Fillup_dummy_module"
 let name_hole_type = "fillup_hole"
+let print_ident id = print_endline @@ Ident.name id
 
 let print_out f x =
   let out = open_out "/tmp/fillup_out.ml" in
@@ -70,54 +71,51 @@ module Fillup_hole = struct
       (fun attr -> attr.attr_name.txt = "instance")
       desc.val_attributes
 
-  let make_instance_list env =
-    let rec loop lvl = function
-      | Env.Env_empty -> []
-      | Env_extension (s, _, _)
-      | Env_modtype (s, _, _)
-      | Env_class (s, _, _)
-      | Env_cltype (s, _, _)
-      | Env_functor_arg (s, _)
-      | Env_type (s, _, _)
-      | Env_constraints (s, _)
-      | Env_module (s, _, _, _)
-      | Env_copy_types s
-      | Env_persistent (s, _)
-      | Env_value_unbound (s, _, _)
-      | Env_module_unbound (s, _, _) ->
-          loop lvl s
-      | Env_value (s, ident, desc) ->
-          if is_instance desc then (ident, desc) :: loop lvl s else loop lvl s
-      | Env_open (s, path) ->
-          let str_items mdecl =
-            match mdecl.Types.md_type with
-            | Mty_signature sg -> sg
-            | Mty_functor _ -> []
-            | _ -> assert false (* --> ここで落ちる *)
-          in
-          let lvl = lvl + 1 in
-          let rest = loop lvl s in
-          let md = Env.find_module path env in
-          List.fold_left
-            (fun res -> function
-              | Types.Sig_value (ident, desc, _) ->
-                  if is_instance desc then (ident, desc) :: loop lvl s
-                  else loop lvl s
-              | _ -> res)
-            rest (str_items md)
+  let make_instances env =
+    let rec search_sg acc = function
+      | [] -> acc
+      | x :: xs -> (
+          match x with
+          | Types.Sig_value (ident, desc, _) ->
+              if
+                is_instance desc
+                (* && Str.string_match
+                     (Str.regexp @@ name_dummy_module ^ "[0-9]+")
+                     (Ident.name ident) 0 *)
+              then search_sg ((ident, desc) :: acc) xs
+              else search_sg acc xs
+          | _ -> search_sg acc xs)
     in
-    loop 0 (Env.summary env)
+    let instances_of_path acc desc =
+      Path.(function Pident id -> (id, desc) :: acc | _ -> acc)
+    in
+    Env.fold_values
+      (fun _ path desc acc ->
+        if is_instance desc then instances_of_path acc desc path else acc)
+      None env []
+    @ Env.fold_modules
+        (fun _ _ decl acc ->
+          match decl.Types.md_type with
+          | Mty_signature sg -> search_sg acc sg
+          | _ -> acc)
+        None env []
+  (* @ Env.fold_modules
+      (fun _ _ decl acc ->
+        match decl.Types.md_type with
+        | Mty_signature sg -> search_sg acc sg
+        | _ -> acc)
+      None env [] *)
 
   let resolve_instances (texp : Typedtree.expression) =
-    let rec find_instances (instances : instance list) =
-      match instances with
-      | (id, desc) :: rest -> (
+    let rec find_instances = function
+      | ((id, desc) : instance) :: rest -> (
           match match_instance texp.exp_env texp.exp_type id desc.val_type with
           | Some i -> i :: find_instances rest
           | None -> find_instances rest)
       | [] -> []
     in
-    find_instances (make_instance_list texp.exp_env)
+    let instances = make_instances texp.exp_env in
+    find_instances instances
 
   let fillup_hole self (super : Untypeast.mapper) attr
       (texp : Typedtree.expression) =
@@ -129,11 +127,11 @@ module Fillup_hole = struct
           | [ Poly (n, ident) ] -> [%expr [%e apply_holes n @@ evar ident]]
           | _ :: _ ->
               Location.raise_errorf ~loc
-                "ppx_fillup Error : Instance overlapped:%a" Printtyp.type_expr
+                "ppx_fillup Error : Instance overlapped : %a" Printtyp.type_expr
                 texp.exp_type
           | [] ->
               Location.raise_errorf ~loc
-                "ppx_fillup Error : Instance not found:%a" Printtyp.type_expr
+                "ppx_fillup Error : Instance not found : %a" Printtyp.type_expr
                 texp.exp_type)
     | _ -> super.expr self texp
 
@@ -227,30 +225,7 @@ module Driver = struct
         let dummy_name = name_dummy_module ^ counter in
         {
           pexp_desc =
-            Pexp_letmodule
-              ( mkloc ~loc @@ Some dummy_name,
-                mod_exp,
-                expr
-                (* {
-                     pexp_desc =
-                       Pexp_open
-                         ( {
-                             popen_expr =
-                               {
-                                 pmod_desc =
-                                   Pmod_ident (mkloc ~loc (Lident dummy_name));
-                                 pmod_loc = loc;
-                                 pmod_attributes = [];
-                               };
-                             popen_override = Fresh;
-                             popen_loc = loc;
-                             popen_attributes = [];
-                           },
-                           expr );
-                     pexp_loc = loc;
-                     pexp_loc_stack = [];
-                     pexp_attributes = [];
-                   } *) );
+            Pexp_letmodule (mkloc ~loc @@ Some dummy_name, mod_exp, expr);
           pexp_loc = loc;
           pexp_loc_stack = [];
           pexp_attributes = [];
