4de35d522c1b6d0598d29b394abe36c36ef685ca
masaki110 <masaki.hmym@google.com>
2022-07-28 13:18:11 +0900

    add expr. in module as instance (doesn't work)

--- src/fillup.ml
+++ src/fillup.ml
@@ -8,6 +8,7 @@ let mark_alert exp =
   let expstr =
     Format.asprintf "ppx_fillup Filled: %a" Pprintast.expression exp
   in
+  let _innerstr exp = Format.asprintf "%a" Pprintast.expression exp in
   let payload = Ast_helper.Exp.constant (Ast_helper.Const.string expstr) in
   let attr =
     {
@@ -15,7 +16,7 @@ let mark_alert exp =
       attr_payload =
         PStr
           [ { pstr_desc = Pstr_eval (payload, []); pstr_loc = exp.pexp_loc } ];
-      attr_loc = Location.none;
+      attr_loc = exp.pexp_loc;
     }
   in
   { exp with pexp_attributes = attr :: exp.Parsetree.pexp_attributes }
@@ -45,19 +46,69 @@ let rec match_instance env holety ident instty =
   | _ -> if Ctype.matches env holety instty then Some (Mono ident) else None
 
 let make_instances env =
-  let ident_of_path =
-    Path.(
-      function
-      | Pident id -> id
-      | Pdot (_, s) -> Ident.create_local s
-      | _ -> assert false)
+  (* let open Path in *)
+  let md_values env =
+    let mds env =
+      Env.fold_modules
+        (fun name _ md acc ->
+          if Str.(string_match (regexp "Fillup_dummy_module") name 0) then (
+            print_endline name;
+            md :: acc)
+          else acc)
+        None env []
+    in
+    let rec str_items md =
+      match md.Types.md_type with
+      | Mty_signature sg -> sg
+      | Mty_functor _ -> []
+      | Mty_alias p | Mty_ident p -> str_items @@ Env.find_module p env
+    in
+    let sgs = List.map str_items (mds env) in
+    let rec loop acc = function
+      | [] -> acc
+      | sg :: rest ->
+          loop
+            (List.fold_left
+               (fun acc -> function
+                 | Types.Sig_value (ident, desc, _) -> (ident, desc) :: acc
+                 | _ -> acc)
+               acc sg)
+            rest
+    in
+    loop [] sgs
   in
-  Env.fold_values
-    (fun _ path desc acc ->
-      if attr_exists desc.val_attributes "instance" then
-        (ident_of_path path, desc) :: acc
-      else acc)
-    None env []
+  (* let rec add_modules env =
+       Types.(
+         function
+         | [] -> env
+         | md :: mds ->
+             let path = path_of_mdtype md.md_type in
+             add_modules
+               (Env.add_module_declaration ~check:false (ident_of_path path)
+                  Mp_present md env)
+               mds)
+     in
+     let env' = add_modules env modules in *)
+  (* prerr_endline
+     @@ string_of_bool
+          (Env.fold_modules (fun name _ _ acc -> name :: acc) None env []
+          = Env.fold_modules (fun name _ _ acc -> name :: acc) None env' []); *)
+  let env_values env =
+    let ident_of_path =
+      Path.(
+        function
+        | Pident id -> id
+        | Pdot (_, s) -> Ident.create_local s
+        | Papply _ -> assert false)
+    in
+    Env.fold_values
+      (fun _ path desc acc ->
+        if attr_exists desc.val_attributes "instance" then
+          (ident_of_path path, desc) :: acc
+        else acc)
+      None env []
+  in
+  env_values env @ md_values env
 
 let resolve_instances (texp : Typedtree.expression) =
   let rec find_instances = function
@@ -88,15 +139,22 @@ let search_hole (super : Untypeast.mapper) (self : Untypeast.mapper)
   | None -> super.expr self texp
   | Some texp -> fillup_hole texp
 
-let rec loop_typer_untyper str =
-  Compmisc.init_path ();
-  let env = Compmisc.initial_env () in
-  let tstr, _, _, _ = Typemod.type_structure env str in
-  let str' = untyp_expr_mapper search_hole tstr in
-  if str = str' then
-    (* print_out (Format.asprintf "%a" Pprintast.structure str'); *)
-    expr_mapper alert_filled str'
-  else loop_typer_untyper str'
+let rec loop_typer_untyper =
+  let cnt = ref 0 in
+  fun str ->
+    cnt := !cnt + 1;
+    Compmisc.init_path ();
+    let env = Compmisc.initial_env () in
+    let tstr, _, _, _ = Typemod.type_structure env str in
+    let str' = untyp_expr_mapper search_hole tstr in
+    if str = str' then (
+      let str' = expr_mapper alert_filled str' in
+      print_out
+      @@ Format.asprintf "%a" Pprintast.structure str'
+      ^ "\n\ntyping & untyping loop : "
+      ^ string_of_int !cnt;
+      str')
+    else loop_typer_untyper str'
 
 let replace_hashhash_with_holes (super : Ast_mapper.mapper)
     (self : Ast_mapper.mapper) exp =
