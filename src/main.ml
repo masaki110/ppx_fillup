57c38ad5103fd17c9f8dcb85daac36cd489e5b3a
masaki110 <masaki.hmym@google.com>
2023-01-28 03:45:10 +0900

    add a syntax for open expressions (doesn't work)

--- src/main.ml
+++ src/main.ml
@@ -13,7 +13,7 @@ let open_instance_toplevel =
     Ast_pattern.(pstr @@ pstr_open __ ^:: nil)
     (fun ~loc ~path:_ md ->
       let md_exp = md.popen_expr in
-      let dummy_name = mk_dummy_module_name () in
+      let dummy_name = mk_dummy_md_name () in
       {
         pstr_desc =
           Pstr_module
@@ -32,13 +32,50 @@ let open_instance_local =
     Ast_pattern.(pstr @@ pstr_eval (pexp_open __ __) nil ^:: nil)
     (fun ~loc ~path:_ md expr ->
       let md_exp = md.popen_expr in
-      let dummy_name = mk_dummy_module_name () in
+      let dummy_name = mk_dummy_md_name () in
       {
         pexp_desc = Pexp_letmodule (mkloc ~loc @@ Some dummy_name, md_exp, expr);
         pexp_loc = loc;
         pexp_loc_stack = [];
         pexp_attributes = [];
       })
+
+(* [%%fillup_open M(a,b,c)], open expressions (a,b,c) in module M as instances *)
+let open_expression =
+  let open Ppxlib.Ast_helper in
+  Extension.declare "fillup_open" Extension.Context.structure_item
+    Ast_pattern.(pstr @@ pstr_eval (pexp_construct __ __) nil ^:: nil)
+    (fun ~loc ~path:_ lid expop ->
+      (* let md_lid_loc = mkloc ~loc lid in *)
+      let expop_of_str = function
+        | Some { pexp_desc = Pexp_tuple l; _ } ->
+            let rec loop acc = function
+              | { pexp_desc = Pexp_ident lid_loc; _ } :: rest ->
+                  rest
+                  |> loop
+                     @@ (Str.eval
+                        @@ Exp.ident
+                        @@ mkloc ~loc
+                        @@ Ldot (lid, Longident.name lid_loc.txt))
+                        :: acc
+              | [] -> acc
+              | _ -> assert false
+            in
+            loop [] l
+        | Some { pexp_desc = Pexp_ident lid_loc; _ } ->
+            [
+              Str.eval
+              @@ Exp.ident
+              @@ mkloc ~loc
+              @@ Ldot (lid, Longident.name lid_loc.txt);
+            ]
+        | None -> []
+        | Some _ ->
+            Location.raise_errorf ~loc "(ppx_fillup) Invalid syntax: %s %a"
+              (Longident.name lid) Pprintast.expression (Option.get expop)
+      in
+      let dummy_md_name = mkloc ~loc @@ Some (mk_dummy_md_name ()) in
+      Str.module_ @@ Mb.mk dummy_md_name @@ Mod.structure (expop_of_str expop))
 
 let transform (str : Parsetree.structure) =
   Fillup.replace_hashhash str
