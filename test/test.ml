5a24c73b4c2ab84f0dd8b31e0ae7cf9da5ebfc25
masaki110 <masaki.hmym@google.com>
2023-03-12 16:46:10 +0900

    modify for ppx_deriving (only show plugin)

--- test/test.ml
+++ test/test.ml
@@ -17,27 +17,26 @@ end
 let (show_option [@instance_with_context]) =
  fun inst -> function None -> "None" | Some i -> "Some " ^ inst i
 
-let (show_list [@instance_with_context]) =
- fun inst xs -> String.concat ", " (List.map inst xs)
+let (print_list [@instance_with_context]) =
+ fun inst xs -> "[" ^ String.concat ", " (List.map inst xs) ^ "]"
 
-let show inst (v : 'a) = inst v
-let (msg [@instance]) = Show.show_msg
+let id x = x
 
 let test_show _ =
   let open%fillup Show in
   let msg = Show.(Msg "msg") in
 
-  assert_equal "123" show ## 123;
-  assert_equal "1.23" show ## 1.23;
-  assert_equal "abc" show ## "abc";
-  assert_equal "true" show ## true;
+  assert_equal "123" id ## 123;
+  assert_equal "1.23" id ## 1.23;
+  assert_equal "abc" id ## "abc";
+  assert_equal "true" id ## true;
 
-  assert_equal "Some 123" show ## (Some 123);
-  assert_equal "None" show ## (None : string option);
-  assert_equal "123, 456, 789" show ## [ 123; 456; 789 ];
-  assert_equal "1.23, 4.56, 7.89" show ## [ [ 1.23; 4.56 ]; [ 7.89 ] ];
+  assert_equal "Some 123" id ## (Some 123);
+  assert_equal "None" id ## (None : string option);
+  assert_equal "[123, 456, 789]" id ## [ 123; 456; 789 ];
+  assert_equal "[[1.23, 4.56], [7.89]]" id ## [ [ 1.23; 4.56 ]; [ 7.89 ] ];
 
-  assert_equal "msg" show##msg;
+  assert_equal "msg" id##msg;
   ()
 
 (* 3 or more arguments *)
@@ -46,8 +45,9 @@ let test_print_ast _ =
   let open! Ppxlib.Parsetree in
   let open%fillup Ppxlib.Pprintast in
   let loc = Location.none in
-  assert_equal "1 + 1" show ## [%expr 1 + 1];
+  assert_equal "1 + 1" id ## [%expr 1 + 1];
   assert_equal "42 : int" (Format.asprintf "%d : %a" 42 __ [%type: int]);
+  Format.printf "%d : %a" 42 __ [%type: int];
   ()
 
 (* add *)
@@ -66,13 +66,66 @@ let test_add _ =
   assert_equal ("foo" ^ "bar") (add __ "foo" "bar");
   ()
 
+(* optional arguments *)
+
+module Eq = struct
+  let eq_int = Int.equal
+  let eq_float = Float.equal
+end
+
+let test_optional _ =
+  let mem = Base.List.mem in
+  let open%fillup Eq in
+  assert_equal true (mem ##~ equal [ 1; 3; 5 ] 1);
+  assert_equal true (mem ##~ equal [ 1; 3; 5 ] 1);
+  (* assert_equal (1.23 +. 4.56) (add __ 1.23 4.56);
+     assert_equal ("foo" ^ "bar") (add __ "foo" "bar"); *)
+  ()
+
+(* first-class module *)
+(* let first_class_module _ = *)
+(* let sort (type s) (module Set : Set.S with type elt = s) l =
+     Set.elements (List.fold_right Set.add l Set.empty)
+
+   (* in *)
+   let make_set (type s) cmp =
+     let module S = Set.Make (struct
+       type t = s
+
+       let compare = cmp
+     end) in
+     (module S : Set.S with type elt = s)
+   (* in *)
+
+   let s = make_set
+   let _ = sort (module ) s *)
+
+(* ppx_deriving *)
+type person = { id : int; name : string } [@@deriving show]
+type affiliate = { name : string } [@@deriving show]
+
+let ppx_deriving _ =
+  let open%fillup Show in
+  (* print_endline ## 123; *)
+  assert_equal "{ id = 013; name = 'ito' }" id ## { id = 013; name = "ito" };
+  assert_equal "{ name = 'Gifu Univ.' }" id ## { name = "Gifu Univ." };
+  ()
+
+let test_list_sort _ =
+  let sort = List.sort [%derive.ord: int * int] in
+  assert_equal ~printer:[%derive.show: (int * int) list]
+    [ (1, 1); (2, 0); (3, 5) ]
+    (sort [ (2, 0); (3, 5); (1, 1) ])
+
 let _ =
   let tests =
     "Test fillup"
     >::: [
-           " show" >:: test_show;
+           " id" >:: test_show;
            " print Ast" >:: test_print_ast;
            " add" >:: test_add;
+           (* " first-class module" >:: first_class_module; *)
+           " ppx_deriving (show)" >:: ppx_deriving;
          ]
   in
   run_test_tt_main tests
