cca92b0c91d4d516cc8b9b4dab5c4cece98c92c4
masaki110 <masaki.hmym@google.com>
2023-10-03 09:52:32 +0900

    delete bench

--- examples/bench/bench_tynest/bench_tynest.ml
+++
@@ -1,5 +0,0 @@
-let (show_list [@instance_with_context]) =
- fun inst xs -> "[" ^ String.concat ", " (List.map inst xs) ^ "]"
-
-let (show_int [@instance]) = string_of_int
-let _ = print_endline ## [ [ [ [ [ [ [ [ [ [ 42 ] ] ] ] ] ] ] ] ] ]
