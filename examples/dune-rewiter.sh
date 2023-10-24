b01e4ed26c3bac8f83c29667cc36b4a6776e0327
masaki110 <masaki.hmym@google.com>
2023-10-24 10:55:27 +0900

    Initial commit

---
+++ examples/dune-rewiter.sh
@@ -0,0 +1,2 @@
+find . -name "dune" -exec perl -p -i -e 's/\((staged_)?pps(?!.*ppx_fillup.*)/\(staged_pps ppx_fillup/sg' {} \;
+find . -name "dune" -exec perl -p -i -e 's/\(libraries(?!.*ppx_fillup.*)/\(libraries ppx_fillup/sg' {} \;
