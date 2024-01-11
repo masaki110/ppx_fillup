5246c2381f5ad9f910e16a6d25b32071d289d798
masaki110 <masaki.hmym@google.com>
2024-01-11 14:09:18 +0900

    delete default instance

--- src/ppx_fillup.ml
+++ src/ppx_fillup.ml
@@ -1,28 +1,6 @@
-(*** default instance ***)
-(* let (addii [@instance ( + )]) = ( + )
-let (addff [@instance ( + )]) = ( +. )
-let (addif [@instance ( + )]) = fun a b -> float_of_int a +. b
-let (addfi [@instance ( + )]) = fun a b -> a +. float_of_int b
-let (subii [@instance ( - )]) = ( - )
-let (subff [@instance ( - )]) = ( -. )
-let (subif [@instance ( - )]) = fun a b -> float_of_int a -. b
-let (subfi [@instance ( - )]) = fun a b -> a -. float_of_int b
-let (mulii [@instance ( * )]) = ( * )
-let (mulff [@instance ( * )]) = ( *. )
-let (mulif [@instance ( * )]) = fun a b -> float_of_int a *. b
-let (mulfi [@instance ( * )]) = fun a b -> a *. float_of_int b
-let (divii [@instance ( / )]) = ( / )
-let (divff [@instance ( / )]) = ( /. )
-let (divif [@instance ( / )]) = fun a b -> float_of_int a /. b
-let (divfi [@instance ( / )]) = fun a b -> a /. float_of_int b
-
-(*** default function ***)
-let show inst x : string = inst x
-let print show x = print_endline @@ show x;; *)
-
 Ppxlib.Driver.(
   register_transformation
-    ~rules:Ppx_fillupsyntax.[ hole; open_instance_toplevel; open_instance_local ]
+    ~rules:Ppx_fillupsyntax.[ open_instance_toplevel; open_instance_local ]
     ~instrument:(Instrument.make Ppx_filluplib.transform ~position:After)
     (* ~impl:Ppx_filluplib.transform *)
     "ppx_fillup")
