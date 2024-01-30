aa552570ec6cf5049bddc269ffc4b649552b488f
masaki110 <masaki.hmym@google.com>
2024-01-30 14:12:48 +0900

    examples of recursion

---
+++ examples/nest/nonest/test.ml
@@ -0,0 +1,15 @@
+let (show [@instance]) = string_of_int
+let (show [@instance]) = string_of_float
+
+let (show [@rec_instance]) =
+  fun xs -> "[" ^ List.fold_left (fun acc x -> acc ^ show x ^ "; ") " " xs ^ "]"
+;;
+
+print_endline @@ show 3;
+print_endline @@ show 3.14
+
+(*
+   without nest
+   benchmark (10 times average)
+   compile : 2.279s
+*)
